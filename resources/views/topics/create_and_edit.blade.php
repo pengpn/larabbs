0b8fb7fa49fe00b3cdbf3334696da08369fc4d6c
pengpn <312924860@qq.com>
2018-04-19 21:57:28 +0800

    修复编辑的时候，分类没有查出和选中

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -38,7 +38,12 @@
                     <select class="form-control" name="category_id" required>
                         <option value="" hidden disabled selected>请选择分类</option>
                         @foreach ($categories as $value)
-                            <option value="{{ $value->id }}">{{ $value->name }}</option>
+                            <option value="{{ $value->id }}"
+                                    @if($value->id == $topic->category_id)
+                                    selected
+                                    @endif>
+                                {{ $value->name }}
+                            </option>
                         @endforeach
                     </select>
                 </div>
