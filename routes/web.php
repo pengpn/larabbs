f80e62af4c5988dc7368ca682f5d92eeac37db36
pengpn <312924860@qq.com>
2018-04-23 21:58:21 +0800

    WYSIWYG Editor

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Illuminate\Support\Arr;
+
 Route::get('/', 'PagesController@root')->name('root');
 
 // Authentication Routes...
@@ -33,3 +35,9 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+
+Route::get('check',function (){
+   return Arr::get(['a' => ['1' => '123'],'b' => ['2' => '234']],'b',[]);
+
+});
