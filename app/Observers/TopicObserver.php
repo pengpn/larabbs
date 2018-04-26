2c090b98bac5959b12176bc984ccd53f4b247ad2
pengpn <312924860@qq.com>
2018-04-26 22:20:26 +0800

    使用队列(若失败则是百度翻译api未设置)Add queue for translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -16,8 +17,26 @@ class TopicObserver
         $topic->excerpt = make_excerpt($topic->body);
 
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
-        if (!$topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+//        if (!$topic->slug) {
+//            //$topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+//            // 推送任务到队列
+//            dispatch(new TranslateSlug($topic));
+//        }
+    }
+
+    /*
+     * 队列系统对于构造器里传入的 Eloquent 模型，
+     * 将会只序列化 ID 字段，因为我们是在 Topic 模型监控器的 saving() 方法中分发队列任务的，
+     * 此时传参的 $topic 变量还未在数据库里创建，所以 $topic->id 为 null。
+     * 所以将代码挪到saved
+     */
+    public function saved(Topic $topic)
+    {
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( ! $topic->slug) {
+
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 
