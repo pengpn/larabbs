b15b96c26cf203d43400bb952fc82158b367633d
pengpn <312924860@qq.com>
2018-07-19 15:26:18 +0800

    L3, 3.3. 手机注册验证码 verificationCode store

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -19,7 +19,6 @@ class VerificationCodesController extends Controller
         } else {
             //生成4位随机数，左侧补0
             $code = str_pad(random_int(1, 9999),4,0,STR_PAD_LEFT);
-
             try {
                 $result = $easySms->send($phone,[
                     'content' => "【彭柯强】您的验证码是{$code}。如非本人操作，请忽略本短信"
@@ -29,16 +28,19 @@ class VerificationCodesController extends Controller
                 return $this->response->errorInternal($message ?? '短信发送异常');
             }
 
-            $key = 'verificationCode_' . str_random(15);
-            $expiredAt = now()->addMinutes(10);
-            //缓存验证码 10分钟过期.
-            Cache::put($key,['phone' => $phone, 'code' => $code], $expiredAt);
-
-            return $this->response->array([
-                'key' => $key,
-                'expired_at' => $expiredAt->toDateTimeString()
-            ])->setStatusCode(201);
         }
+
+
+
+        $key = 'verificationCode_' . str_random(15);
+        $expiredAt = now()->addMinutes(10);
+        //缓存验证码 10分钟过期.
+        Cache::put($key,['phone' => $phone, 'code' => $code], $expiredAt);
+
+        return $this->response->array([
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString()
+        ])->setStatusCode(201);
 
     }
 }
