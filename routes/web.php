af591299d7f2c2019552e5f70302539d928649b0
pengpn <312924860@qq.com>
2018-07-19 10:18:48 +0800

    8.5. 站点首页 Setup default home page

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 use Illuminate\Support\Arr;
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 // Authentication Routes...
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
