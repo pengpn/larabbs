977160f772a6c09578a8f3b09a1d66de72a6320f
pengpn <312924860@qq.com>
2018-07-20 13:49:32 +0800

    L3, 5.2. 获取个人信息 api/user

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,8 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
+    'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
 ], function($api) {
 
     $api->group([
@@ -23,6 +24,8 @@ $api->version('v1', [
         'limit' => config('api.rate_limits.sign.limit'),
         'expires' => config('api.rate_limits.sign.expires'),
     ], function ($api){
+        //游客可以访问的接口
+
         // 短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store')
             ->name('api.verificationCodes.store');
@@ -50,6 +53,15 @@ $api->version('v1', [
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
+
+
+
+        // 需要token验证的接口
+        $api->group(['middleware' => 'api.auth'],function($api){
+            //当前登录用户的信息
+            $api->get('user','UsersController@me')
+                ->name('api.user.show');
+        });
     });
 
 });
