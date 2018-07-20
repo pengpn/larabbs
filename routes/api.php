3a05c41a306c8e1302a70db767cc42d2c3a7d580
pengpn <312924860@qq.com>
2018-07-20 16:57:26 +0800

    L3, 6.3. 修改话题 topic update api

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
-    'middleware' => 'serializer:array'
+    'middleware' => ['serializer:array', 'bindings']
 ], function($api) {
 
     $api->group([
@@ -77,7 +77,9 @@ $api->version('v1', [
             // 发布话题
             $api->post('topics', 'TopicsController@store')
                 ->name('api.topics.store');
-
+            // 修改话题
+            $api->patch('topics/{topic}', 'TopicsController@update')
+                ->name('api.topics.update');
 
         });
     });
