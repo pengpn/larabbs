534e6521cb0b7418e1f1ebf8d8c86f88b52a2878
pengpn <312924860@qq.com>
2018-07-20 10:45:22 +0800

    L3, 4.4. 微信登录功能开发 social authorizations

---
+++ app/Http/Requests/Api/SocialAuthorizationRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SocialAuthorizationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        //要么提交授权码code，要么提交access_token和openid
+        $rules = [
+            'code' => 'required_without:access_token|string',
+            'access_token' => 'required_without:code|string',
+        ];
+
+        if ($this->social_type == 'weixin' && !$this->code) {
+            $rules['openid']  = 'required|string';
+        }
+
+
+        return $rules;
+    }
+}
