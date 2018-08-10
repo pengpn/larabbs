a64378d60526c21839941b9d1225f5d697d4dd37
pengpn <312924860@qq.com>
2018-08-10 10:39:25 +0800

    L3, 7.4. 消息通知列表 notifications index

--- routes/api.php
+++ routes/api.php
@@ -70,6 +70,12 @@ $api->version('v1', [
         // 某个用户的回复列表
         $api->get('users/{user}/replies', 'RepliesController@userIndex')
             ->name('api.users.replies.index');
+        // 删除回复
+        $api->delete('topics/{topic}/replies/{reply}','RepliesController@destory')
+            ->name('api.topics.replies.destroy');
+        // 通知列表
+        $api->get('user/notifications','NotificationsController@index')
+            ->name('api.user.notifications.index');
 
 
 
