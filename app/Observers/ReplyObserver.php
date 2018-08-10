ca5db3a99fbb4b5d7b5acaec1bb37775ae5651f5
pengpn <312924860@qq.com>
2018-08-10 11:00:25 +0800

    L3,7.5. 未读消息统计 notifications stats

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -16,7 +16,10 @@ class ReplyObserver
         $reply->topic->increment('reply_count', 1);
 
         //通知作者话题被回复了
-        $topic->user->notify(new TopicReplied($reply));
+        // 如果评论的作者不是话题的作者，才需要通知
+        if ( ! $reply->user->isAuthorOf($topic)) {
+            $topic->user->notify(new TopicReplied($reply));
+        }
     }
 
     public function creating(Reply $reply)
