9578657e2ce96778830a14a7786d32b66ea6d8bc
pnpeng <iopkq999@gmail.com>
2019-04-24 18:38:05 +0800

    test 2

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -21,8 +21,8 @@ class TestController extends Controller
         return view('pages.permission_denied');
     }
 
-    public function test()
+    public function test2()
     {
-        echo 'test';
+        echo 'test2';
     }
 }
